b309c110fa3d0ff93f77bf5ab78bbbddc4a36ee1
Cj Karinen <Cjkarinen20@gmail.com>
2024-10-17 17:09:47 -0400

    Gun Framework (Needs Recoil)
    -Created gun framework
    -Had camera recoil but removed it because it caused issues

--- Assets/CJ's Character Controller/Scripts/Guns/Framework/GunData.cs
+++ Assets/CJ's Character Controller/Scripts/Guns/Framework/GunData.cs
@@ -17,4 +17,17 @@ public class GunData : ScriptableObject
     [Header("Reload Config")]
     public float magazineSize;
     public float reloadTime;
+
+    [Header("Aim Recoil Settings")]
+    public float a_recoilAmount;
+    public Vector2 a_maxRecoil;
+    public float a_recoilSpeed;
+    public float a_resetRecoilSpeed;
+
+    [Header("Directional Recoil Settings")]
+    public float d_recoilAmount;
+    public Vector2 d_maxRecoil;
+    public float d_recoilSpeed;
+    public float d_resetRecoilSpeed;
+
 }
